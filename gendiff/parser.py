5d14b9c31d77626ccb0cca5661baf7846e22cedc
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2022-12-29 01:26:37 +0300

    Update parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -10,8 +10,8 @@ def read_file(file_path):
     return file_data, file_extension
 
 
-def parse(file_data, file_extension='.json'):
+def parse(data, file_extension='.json'):
     if file_extension == '.json':
-        return json.loads(file_data)
+        return json.loads(data)
     if file_extension == '.yml' or '.yaml':
-        return yaml.load(file_data, Loader=yaml.FullLoader)
+        return yaml.load(data, Loader=yaml.FullLoader)
